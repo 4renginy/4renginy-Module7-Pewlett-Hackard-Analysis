441acbe324af185472e7f924b131084c5470166e
Rengin <4renatit@gmail.com>
2020-11-09 13:26:01 -0800

    update

--- Analysis Projects Folder/Pewlett-Hackard Analysis Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard Analysis Folder/Queries/Employee_Database_Challenge.sql
@@ -4,7 +4,7 @@ SELECT e.emp_no,
     t.title,
 	t.from_date,
 	t.to_Date
-INTO retirement_titles
+-- retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -18,7 +18,7 @@ SELECT DISTINCT ON (emp_no ) rt.emp_no,
 							rt.first_name,
 							rt.last_name,
 							rt.title
-				INTO unique_titles
+				--INTO unique_titles
 				FROM retirement_titles as rt
 				ORDER BY rt.emp_no asc, to_date desc ;
 
@@ -30,19 +30,25 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC;
 
+_____________ADDITIONAL QUERIES________________
+			CURRENT RETIREEES QUERY
 
-SELECT DISTINCT ON (e.emp_no )e.emp_no,
-    e.first_name,
-	e.last_name,
-	e.birth_date,
-    de.from_date,
-	de.to_date,
-	ti.title
-INTO mentorship_eligibilty
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles as ti
-ON (e.emp_no= ti.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
-ORDER BY(e.emp_no);
+SELECT DISTINCT ON (emp_no ) rt.emp_no,
+							rt.first_name,
+							rt.last_name,
+							rt.title,
+							rt.from_date,
+							rt.to_date
+				INTO rettit_current
+				FROM retirement_titles as rt
+				where (to_date='9999/01/01')
+				ORDER BY rt.emp_no asc, to_date desc ;
+				
+select count(rettit_current.emp_no), rettit_current.title from rettit_current
+group by rettit_current.title
+order by rettit_current.title;
+
+
+
+
+	
